f919df2f895fccec657fdae9f713738fbd33fb95
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2023-12-13 00:38:39 +0530

    commit 31

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -9,7 +9,7 @@ export async function POST(req, res) {
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: email,
+      to: ["nathanseueira425@gmail.com"],
       subject: subject,
       react: (
         <>
