72c8bc0ce76a046d805d7cdb792feaa69bd8e9e9
The_Ultimate_Sequeira <117172083+Nathan-sudo-pycharm@users.noreply.github.com>
2023-12-13 00:10:11 +0530

    commit 26

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-5xl text-white font-semibold"
+          className="text-xl md:text-2xl text-white font-semibold"
         >
           Nathan Sequeira
         </Link>
